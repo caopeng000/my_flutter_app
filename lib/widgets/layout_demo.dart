bdba431f02521fde00710650e62ca5d176ddfa4e
caopeng <caopeng@chehejia.com>
2021-05-14 17:50:02 +0800

    feat:流式布局

--- lib/widgets/layout_demo.dart
+++ lib/widgets/layout_demo.dart
@@ -11,6 +11,7 @@ class LayoutDemo extends StatelessWidget {
       ),
       body: Container(
         color: Colors.grey,
+        child: WrapDemo(),
         // child: Column(
         //   children: [
         //     Container(color: Colors.red, width: 100, height: 100),
@@ -30,25 +31,43 @@ class LayoutDemo extends StatelessWidget {
         //   ],
         // ),
 
-        child: Flex(
-          direction: Axis.vertical,
-          children: [
-            Expanded(
-              child: Container(color: Colors.red, width: 100, height: 200),
-              flex: 1,
-            ),
-            Expanded(
-              child: Container(color: Colors.green, width: 100, height: 200),
-              flex: 2,
-            ),
-            Expanded(
-              child: Container(color: Colors.yellow, width: 100, height: 200),
-              flex: 1,
-            ),
-
-          ],
-        ),
+        //
       ),
+    );
+  }
+}
+
+class WrapDemo extends StatefulWidget {
+  const WrapDemo({Key key}) : super(key: key);
+
+  @override
+  _WrapDemoState createState() => _WrapDemoState();
+}
+
+class _WrapDemoState extends State<WrapDemo> {
+  List<int> list = List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (var i = 0; i < 20; i++) {
+      list.add(i);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 10.0,
+     direction: Axis.vertical,
+     alignment: WrapAlignment.start,
+     runSpacing: 1.0,
+     children: list.map((e) => Container(
+       height: 100,
+       width: 100,
+       child: Text(e.toString()),
+     )).toList(),
     );
   }
 }
