5602f221b94386eb1bbdc0603dd5c8c72cd0b44a
caopeng <caopeng@chehejia.com>
2021-05-17 15:37:24 +0800

    feat:学习datatable

--- lib/widgets/table_demo.dart
+++ lib/widgets/table_demo.dart
@@ -8,35 +8,48 @@ class TableDemo extends StatefulWidget {
 }
 
 class _TableDemoState extends State<TableDemo> {
-  List<Map> list=new List();
+  List<Map> list = new List();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    for(var i=0;i<10;i++){
+    for (var i = 0; i < 10; i++) {
       list.add({
-        "name":"wang"+i.toString(),
-        "gender":i%1==0?"男":"女",
+        "name": "wang" + i.toString(),
+        "gender": i % 1 == 0 ? "男" : "女",
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("表格演示"),
-        ),
+      appBar: AppBar(
+        title: Text("表格演示"),
+      ),
       body: Padding(
         padding: EdgeInsets.all(10),
-        child: Table(
-          border: TableBorder.all(
-              color: Colors.grey
-          ),
-          children: list.map((e) => TableRow(
-              children: [
-                Text(e["name"]),
-                Text(e["gender"])
+        // child: Table(
+        //   border: TableBorder.all(
+        //       color: Colors.grey
+        //   ),
+        //   children: list.map((e) => TableRow(
+        //       children: [
+        //         Text(e["name"]),
+        //         Text(e["gender"])
+        //       ]
+        //   )).toList(),
+        // ),
+        child: DataTable(
+          columns: [
+            DataColumn(label: Text("姓名")),
+            DataColumn(label: Text("性别"))
+          ],
+          rows: list.map((e) => DataRow(
+              cells: [
+                DataCell(Text(e["name"])),
+                DataCell(Text(e["gender"])),
               ]
           )).toList(),
         ),
