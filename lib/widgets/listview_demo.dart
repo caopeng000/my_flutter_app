e790af6f8fd3cd4f17b1f859087993fff7a18043
caopeng <caopeng@chehejia.com>
2021-05-17 14:30:33 +0800

    feat:学习了listview以及置顶动画

--- lib/widgets/listview_demo.dart
+++ lib/widgets/listview_demo.dart
@@ -9,14 +9,36 @@ class ListViewDemo extends StatefulWidget {
 
 class _ListViewDemoState extends State<ListViewDemo> {
   List<int> list = List();
+  ScrollController _controller;
+  bool show = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _controller = ScrollController();
+    _controller.addListener(() {
+      if (_controller.offset >= 100 && show == false) {
+        setState(() {
+          show = true;
+        });
+      } else if (_controller.offset < 100 && show == true) {
+        setState(() {
+          show = false;
+        });
+      }
+      print(_controller);
+    });
     for (var i = 0; i < 100; i++) {
       list.add(i);
     }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
@@ -25,11 +47,39 @@ class _ListViewDemoState extends State<ListViewDemo> {
       appBar: AppBar(
         title: Text("滚动列表"),
       ),
-      body:Scrollbar(
-        child:  ListView(
-          children: list.map((e) => Text(e.toString())).toList(),
+      floatingActionButton: show
+          ? FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _controller.animateTo(
+              0, duration: Duration(microseconds: 300), curve: Curves.ease);
+        },
+      )
+          : null,
+      body: Scrollbar(
+        // child: RefreshIndicator(
+        //   child: ListView(
+        //     children: list.map((e) => Text(e.toString())).toList(),
+        //     // reverse: true,
+        //     itemExtent: 50,
+        //   ),
+        //   onRefresh: _onRefresh,
+        // ),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Text(list[index].toString());
+          },
+          itemCount: list.length,
+          controller: _controller,
         ),
       ),
     );
   }
+}
+
+Future _onRefresh() async {
+  await Future.delayed(Duration(seconds: 3), () {
+    print("object");
+  });
+  return "";
 }
