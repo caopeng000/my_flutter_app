b4d5049981a1d29aa4df209988d6822138ff065e
caopeng <caopeng@chehejia.com>
2021-05-17 10:40:02 +0800

    feat:学习底部导航栏进阶

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/widgets/demo/demo_04.dart';
+import 'package:my_flutter_app/widgets/layout_demo.dart';
 
 class BottomNavigatorBarDemo extends StatefulWidget {
-  const BottomNavigatorBarDemo({Key key}) : super(key: key);
+  List<Widget> widgets=[
+    PageDemo(),
+    LayoutDemo()
+  ];
+
 
   @override
   _BottomNavigatorBarDemoState createState() => _BottomNavigatorBarDemoState();
@@ -12,12 +18,15 @@ class _BottomNavigatorBarDemoState extends State<BottomNavigatorBarDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("底部选项卡"),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   title: Text("底部选项卡"),
+      //   centerTitle: true,
+      // ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: [
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "新增"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "我的"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "新增"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "我的")
         ],
@@ -28,8 +37,27 @@ class _BottomNavigatorBarDemoState extends State<BottomNavigatorBarDemo> {
           });
         },
       ),
+      // bottomNavigationBar: BottomAppBar(
+      //   color:Colors.blue,
+      //   shape: CircularNotchedRectangle(),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     children: [
+      //       IconButton(icon: Icon(Icons.add), onPressed: (){}),
+      //       SizedBox(),
+      //       IconButton(icon: Icon(Icons.home), onPressed: (){}),
+      //     ],
+      //   ),
+      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.widgets_rounded),
+        onPressed: (){
+          print("body");
+        },
+      ),
       body: Center(
-        child: Text(_index.toString()),
+        child:widget.widgets[_index],
       ),
     );
   }
