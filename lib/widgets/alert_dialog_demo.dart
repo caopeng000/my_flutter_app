a6710441752cfb0ec3a0d813d607b0776b852f7c
caopeng <caopeng@chehejia.com>
2021-05-17 15:24:36 +0800

    feat:学习列表对话框

--- lib/widgets/alert_dialog_demo.dart
+++ lib/widgets/alert_dialog_demo.dart
@@ -9,6 +9,17 @@ class AlertDialogDemo extends StatefulWidget {
 }
 
 class _AlertDialogDemoState extends State<AlertDialogDemo> {
+  List<int> list = new List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (var i = 0; i < 20; i++) {
+      list.add(i);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,27 +31,57 @@ class _AlertDialogDemoState extends State<AlertDialogDemo> {
           RaisedButton(
             onPressed: _showAlert,
             child: Text("对话框"),
+          ),
+          RaisedButton(
+            onPressed: _showListDialog,
+            child: Text("列表对话框"),
           )
         ],
       ),
     );
   }
 
-  void _showAlert() async{
-    var result= await  showDialog(context: context, builder: (BuildContext context) {
-      return CupertinoAlertDialog(
-        title: Text("标题"),
-        content: Text("确认删除吗？"),
-        actions: [
-          FlatButton(onPressed: () {
-            Navigator.of(context).pop(false);
-          }, child: Text("确认")),
-          FlatButton(onPressed: () {
-            Navigator.pop(context,true);
-          }, child: Text("删除")),
-        ],
-      );
-    });
+  void _showAlert() async {
+    var result = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text("标题"),
+            content: Text("确认删除吗？"),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: Text("确认")),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child: Text("删除")),
+            ],
+          );
+        });
+    print(result);
+  }
+
+  void _showListDialog() async {
+    var result = await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text("标题"),
+            children: list
+                .map((e) => GestureDetector(
+                      child: Text(e.toString()),
+                      onTap: () {
+                        Navigator.pop(context, e);
+                      },
+                    ))
+                .toList(),
+          );
+        });
     print(result);
   }
 }
